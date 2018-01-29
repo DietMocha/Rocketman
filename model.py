80598fca0afd2be9de5597425dab20a7b28704f2
blane07 <brendan.lane07@gmail.com>
2018-01-28 21:52:33 -0500

    Developing polar coordinates

--- model.py
+++ model.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import rocket, planets
 
 # Initialize your rocket with design parameters and starting conditions
-vehicle = rocket.Rocket(31, 120, propellant_mass_fraction=0.80, mixture_ratio=7.4, tank_pressure=7e6, tank_safety_factor=1.2)
+vehicle = rocket.Rocket(31, 90, altitude=22000, propellant_mass_fraction=0.80, mixture_ratio=7.4, tank_pressure=7e6, tank_safety_factor=1.2)
 
 # Run model
-vehicle.calc(120)    # Method's parameter is the length of simulation in seconds
+vehicle.calc(700)    # Method's parameter is the length of simulation in seconds
 
 # Extract data from flight log
 time = [row[0] for row in vehicle.flight_log]
@@ -18,16 +18,23 @@ mass = [row[6] for row in vehicle.flight_log]
 
 # Plot results
 f, ax = plt.subplots(2, 3)
+
 ax[0, 0].plot(time, altitude)
 ax[0, 0].set_title('Altitude [m]')
+
 ax[0, 1].scatter(time, velocity_radial)
 ax[0, 1].set_title('Radial Velocity [m/s]')
+
 ax[0, 2].scatter(time, drag)
 ax[0, 2].set_title('Drag [N]')
+
 ax[1, 0].plot(time, acceleration_radial)
 ax[1, 0].set_title('Radial Acceleration [g]')
+
 ax[1, 1].scatter(time, thrust)
 ax[1, 1].set_title('Thrust [N]')
+
 ax[1, 2].scatter(time, mass)
 ax[1, 2].set_title('Mass [kg]')
+
 plt.show()
--- rocket.py
+++ rocket.py
@@ -4,22 +4,23 @@ class Rocket(object):
 	""" Describes a 1-stage rocket vehicle and start conditions. Use METRIC units [kg, m, s, Pa, N, K]. """
 
 	def __init__(self, mass, burn_time, flight_time=0, time_step=0.01, propellant_mass_fraction=0.87, altitude=0, velocity_radial=0,
-				 velocity_angular=0, fuel='RP-1', oxidizer='H2O2_98%', mixture_ratio=5, g_limiter=None, isp=0,
-				 number_of_thrusters=1, feed_system='pressure-fed', tank_pressure=0, tank_material='Al_6061_T6',
-				 tank_safety_factor=2, drag_coefficent = 0.30):
+				 velocity_tangential=0, fuel='RP-1', oxidizer='H2O2_98%', mixture_ratio=5, g_limiter=None, isp=0, number_of_thrusters=1, 
+				 feed_system='pressure-fed', tank_pressure=0, tank_material='Al_6061_T6', tank_safety_factor=2, drag_coefficent = 0.30,
+				 angle=0):
 
 		# Starting conditions
 		self.flight_log = []
 		self.flight_time = 0    # Rocket lifts-off at flight_time = 0 seconds
 		self.time_step = time_step
-		self.altitude = altitude    # Height above the surface
-		# self.angle = 0    # The angle between the rocket and the launch site with respect to the Earth's center
+		self.altitude = altitude    # Height above the surface\
+		self.horizontal = 0    # Arc distance travelled down range
+		self.angle = 0    # Zero radians means the rocket is pointing away from the center of the Earth and aligned with the radial axis | pi/2 radians is when the rocket is aligned with the tangential axis
 		self.velocity_radial = velocity_radial
-		self.velocity_angular = velocity_angular + Earth.velocity_angular
-		self.velocity_total = (self.velocity_radial**2 + self.velocity_angular**2) ** 0.5
+		self.velocity_tangential = velocity_tangential + Earth.velocity_angular * Earth.radius
+		self.velocity_total = (self.velocity_radial**2 + self.velocity_tangential**2) ** 0.5
 		self.acceleration_radial = 0
-		# self.acceleration_angular = 
-		# self.acceleration_total = (self.acceleration_radial**2 + self.acceleration_angular**2) ** 0.5
+		self.acceleration_tangential = 0
+		self.acceleration_total = (self.acceleration_radial**2 + self.acceleration_tangential**2) ** 0.5
 		self.g = 9.805 * (Earth.radius / (self.altitude + Earth.radius)) ** 2    # Earth's gravitational constant, which depends on altitude
 
 		# Mass
@@ -29,6 +30,7 @@ class Rocket(object):
 		self.propellant_mass = self.mass - self.dry_mass
 		self.oxidizer_mass = (mixture_ratio * self.propellant_mass) / (mixture_ratio + 1)
 		self.fuel_mass = self.propellant_mass / (mixture_ratio + 1)
+		self.residual_fuel_mass = mass*propellant_mass_fraction*0.02   # Needs development, assuming 2% residual (unused fuel)
 
 		# Engine
 		self.fuel = fuel
@@ -68,20 +70,30 @@ class Rocket(object):
 		self.g = 9.805 * (Earth.radius / (self.altitude + Earth.radius)) ** 2
 
 	def calc_mass(self):
-		self.oxidizer_mass -= self.oxidizer_flow_rate * self.time_step
-		self.fuel_mass -= self.fuel_flow_rate * self.time_step
-		self.propellant_mass = self.oxidizer_mass + self.fuel_mass
-		self.mass = self.dry_mass + self.propellant_mass
+		if self.propellant_mass > self.residual_fuel_mass:   # Prevents mass reduction after rocket uses all available fuel
+			self.oxidizer_mass -= self.oxidizer_flow_rate * self.time_step
+			self.fuel_mass -= self.fuel_flow_rate * self.time_step
+			self.propellant_mass = self.oxidizer_mass + self.fuel_mass
+			self.mass = self.dry_mass + self.propellant_mass
 
-	def calc_acceleration_radial(self):
-		self.drag = 0.5 * self.drag_coefficent * atm_air_density[atm_height.index(round(self.altitude, -2))] * (self.velocity_radial)**2 * self.frontal_area_sphere
-		self.acceleration_radial = ((self.thrust - self.drag) / self.mass) - self.g
+	def calc_acceleration(self):
+		self.thrust = self.thrust if self.propellant_mass > self.residual_fuel_mass else 0
+		drag_velocity = ( (self.velocity_tangential - Earth.velocity_angular * Earth.radius) ** 2 + self.velocity_radial ** 2) ** 0.5    # Velocity that contributes to drag
+		self.drag = (0.5 * self.drag_coefficent * atm_air_density[atm_height.index(round(self.altitude, -2))] * drag_velocity**2 * self.frontal_area_sphere) if self.altitude <= 80000 else 0
+		self.drag = self.drag if self.velocity_total > 0 else -self.drag
+		acceleration = (self.thrust - self.drag) / self.mass
+		self.acceleration_radial = acceleration * math.cos(self.angle) - self.g + (self.velocity_tangential ** 2 / (self.altitude + Earth.radius))    # Radial acceleration from thrust - Earth's gravitational acceleration + centripetal acceleration
+		self.acceleration_tangential = acceleration * math.cos(self.angle)
+		self.acceleration_total = (self.acceleration_tangential ** 2 + self.acceleration_radial ** 2) ** 0.5
 
-	def calc_velocity_radial(self):
+	def calc_velocity(self):
 		self.velocity_radial += self.acceleration_radial * self.time_step
+		self.velocity_tangential += self.acceleration_tangential * self.time_step
+		self.velocity_total = (self.velocity_radial ** 2 + self.velocity_tangential ** 2) ** 0.5
 
-	def calc_altitude(self):
+	def calc_movement(self):
 		self.altitude += self.velocity_radial * self.time_step
+		self.horizontal += self.velocity_tangential * self.time_step
 
 	def calc_time(self):
 		self.flight_time += self.time_step
@@ -90,18 +102,18 @@ class Rocket(object):
 		for time_increment in range(int(time / self.time_step)):
 			self.flight_log.append([self.flight_time, self.altitude, self.velocity_radial, self.acceleration_radial, self.thrust, self.drag, self.mass])
 			self.calc_mass()
-			self.calc_acceleration_radial()
-			self.calc_velocity_radial()
-			self.calc_altitude()
+			self.calc_acceleration()
+			self.calc_velocity()
+			self.calc_movement()
 			self.calc_time()
 
 # Loads the US Standard Atmosphere Table
 # Needs improvements, only goes up to 80,000 meters and need to increase table's resolution
 with open('US_standard_atmosphere.csv') as csvfile:
 	std_atm = [row for row in csv.reader(csvfile, delimiter='\t')]    # Each row contains height above surface, temperature, pressure, density of air
-atm_height = [float(row[0]) for row in std_atm]
+atm_height =      [float(row[0]) for row in std_atm]
 atm_temperature = [float(row[1]) for row in std_atm]
-atm_pressure = [float(row[2]) for row in std_atm]
+atm_pressure =    [float(row[2]) for row in std_atm]
 atm_air_density = [float(row[3]) for row in std_atm]
 
 # Planet reference information
@@ -110,4 +122,4 @@ Earth = planets.Earth()
 # Reference dictionaries
 oxidizer_density = {'H2O2_98%': 1430}    # kg/m3
 fuel_density = {'RP-1': 810}    # kg/m3
-material = {'Al_6061_T6': [240e6, 2700]}   # name: [yield strength (Pa), density (kg/m3)]
+material = {'Al_6061_T6': [240e6, 2700]}   # { name: [yield strength (Pa), density (kg/m3)] }
